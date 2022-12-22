7c9aea1630e1fbd1e19ad046e8719e8b72b63c1c
harshalthanki <90134556+harshalthanki@users.noreply.github.com>
2022-12-22 18:01:02 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,27 +20,39 @@ star_table = soup.find('table')
 temp_list=[]
 
 table_rows = star_table.find_all('tr')
+
 for tr in table_rows:
+    
     td = tr.find_all('td')
+    
     row = [i.text.rstrip() for i in td]
+    
     temp_list.append(row)
 
-
-
 Star_names = []
+
 Distance =[]
+
 Mass = []
+
 Radius =[]
-Lum = []
+
+Luminosity = []
 
 for i in range(1,len(temp_list)):
+    
     Star_names.append(temp_list[i][1])
+    
     Distance.append(temp_list[i][3])
+    
     Mass.append(temp_list[i][5])
+    
     Radius.append(temp_list[i][6])
-    Lum.append(temp_list[i][7])
+    
+    Luminosity.append(temp_list[i][7])
     
 df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+
 print(df2)
 
 df2.to_csv('bright_stars.csv')
